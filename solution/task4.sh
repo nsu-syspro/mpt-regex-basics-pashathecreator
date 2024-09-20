16a3081a1480925c6a691e30d2c9a26d70264b04
Pasha <zzzkelrath@yandex.ru>
2024-09-20 18:57:12 +0700

    Fixed 4th and 5th task

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,2 +1,2 @@
 sed 's/Живи/Не спи/g' data/blok.txt > data/blok_kids_edition.txt
-sed 's/Умрешь/Уснешь/g' data/blok.txt > data/blok_kids_edition.txt
+sed 's/Умрёшь/Уснёшь/g' data/blok.txt > data/blok_kids_edition.txt
